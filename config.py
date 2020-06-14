d21d0248198f71bb8bcf0a4b020cf9ca3cca58df
Mahdi AlKhalaf <m@mahdi.pro>
2020-06-14 23:27:05 +0300

    feat: Configure database connection

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -7,7 +8,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
+url = os.environ.get('FYYUR_POSTGRES_URL')
+if url is not None and len(url) > 0:
+    SQLALCHEMY_DATABASE_URI = url
+else:
+    SQLALCHEMY_DATABASE_URI = 'postgres://postgres:password@localhost:5432/fyyur'
 
-
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+# Disable tracking modifications to eliminate warning
+SQLALCHEMY_TRACK_MODIFICATIONS = False
