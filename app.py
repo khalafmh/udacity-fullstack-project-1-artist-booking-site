7938b7e67ebb15c2519fa31d9c68c65b09fa2a3f
Mahdi AlKhalaf <m@mahdi.pro>
2020-06-16 15:56:07 +0300

    feat: implement the `search_venues()` function

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
+from sqlalchemy import func
 
 from forms import *
 
@@ -134,16 +135,16 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for Hop should return "The Musical Hop".
-    # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+    search = request.form.get('search_term')
+    venues = Venue.query.filter(func.lower(Venue.name).like(f"%{search.lower()}%")).all()
+
     response = {
-        "count": 1,
+        "count": len(venues),
         "data": [{
-            "id": 2,
-            "name": "The Dueling Pianos Bar",
-            "num_upcoming_shows": 0,
-        }]
+            "id": venue.id,
+            "name": venue.name,
+            "num_upcoming_shows": len(get_upcoming_shows(venue.shows)),
+        } for venue in venues]
     }
     return render_template('pages/search_venues.html', results=response,
                            search_term=request.form.get('search_term', ''))
