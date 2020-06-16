0d3ebfa555bfd9dd88b0c61203b0bec7290ddfa3
Mahdi AlKhalaf <m@mahdi.pro>
2020-06-16 14:04:52 +0300

    test: Add script for inserting dummy data

---
+++ test/insert_dummy_data.sql
@@ -0,0 +1,45 @@
+INSERT INTO "Venue" (id, name, city, state, address, phone, genres, image_link, facebook_link, website, seeking_talent,
+                     seeking_description)
+VALUES (1, 'The Musical Hop', 'San Francisco', 'CA', '1015 Folsom Street', '123-123-1234',
+        '["Jazz", "Reggae", "Swing", "Classical", "Folk"]',
+        'https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+        'https://www.facebook.com/TheMusicalHop',
+        'https://www.themusicalhop.com',
+        TRUE, 'We are on the lookout for a local artist to play every two weeks. Please call us.'),
+       (2, 'The Dueling Pianos Bar', 'New York', 'NY', '335 Delancey Street', '914-003-1132',
+        '["Classical", "R&B", "Hip-Hop"]',
+        'https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+        'https://www.facebook.com/theduelingpianos',
+        'https://www.theduelingpianos.com',
+        FALSE, ''),
+       (3, 'Park Square Live Music & Coffee', 'San Francisco', 'CA', '34 Whiskey Moore Ave', '415-000-1234',
+        '["Rock n Roll", "Jazz", "Classical", "Folk"]',
+        'https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80',
+        'https://www.facebook.com/ParkSquareLiveMusicAndCoffee',
+        'https://www.parksquarelivemusicandcoffee.com',
+        FALSE, '');
+
+INSERT INTO "Artist" (id, name, city, state, phone, genres, image_link, facebook_link, seeking_description,
+                      seeking_venue, website)
+VALUES (4, 'Guns N Petals', 'San Francisco', 'CA', '326-123-5000', '["Rock n Roll"]',
+        'https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+        'https://www.facebook.com/GunsNPetals',
+        'Looking for shows to perform at in the San Francisco Bay Area!', TRUE,
+        'https://www.gunsnpetalsband.com'),
+       (5, 'Matt Quevedo', 'New York', 'NY', '300-400-5000', '["Jazz"]',
+        'https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+        'https://www.facebook.com/mattquevedo923251523',
+        '', FALSE,
+        ''),
+       (6, 'The Wild Sax Band', 'San Francisco', 'CA', '432-325-5432', '["Jazz", "Classical"]',
+        'https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80',
+        '',
+        '', FALSE,
+        '');
+
+INSERT INTO "Show" (venue_id, artist_id, start_time)
+VALUES (1, 4, '2019-05-21T21:30:00.000Z'),
+       (3, 5, '2019-06-15T23:00:00.000Z'),
+       (3, 6, '2035-04-01T20:00:00.000Z'),
+       (3, 6, '2035-04-08T20:00:00.000Z'),
+       (3, 6, '2035-04-15T20:00:00.000Z');
